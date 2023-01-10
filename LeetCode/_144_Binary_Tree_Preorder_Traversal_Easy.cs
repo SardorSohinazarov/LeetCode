fc03d065e0b70fce98b338eb31bb73eee21991e0
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-01-10 19:58:58 +0500

    144 yechildi

---
+++ LeetCode/_144_Binary_Tree_Preorder_Traversal_Easy.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode144;//solved
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    public IList<int> PreorderTraversal(TreeNode root)
+    {
+        List<int> list = new List<int>();
+
+        Iterate(root);
+
+        void Iterate(TreeNode node){
+            if(node != null)
+            {
+                list.Add(node.val);
+                Iterate(node.left);
+                Iterate(node.right);
+            }
+        }
+
+        return list;
+    }
+}
