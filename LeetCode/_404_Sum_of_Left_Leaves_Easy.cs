03bdc2d065c989fbb3366b31d1979cc3aded30fe
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-01-18 15:47:57 +0500

    404 solved

---
+++ LeetCode/_404_Sum_of_Left_Leaves_Easy.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode144;//solved
+
+namespace LeetCode404
+{
+    public class Solution
+    {
+        public int SumOfLeftLeaves(TreeNode root)
+        {
+            int sum = 0;
+
+            Sardor(root);
+
+            void Sardor(TreeNode root)
+            {
+                if (root is not null)
+                {
+                    if (root.left is not null && root.left.left == null && root.left.right == null)
+                        sum += root.left.val;
+                    Sardor(root.left);
+                    Sardor(root.right);
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ LeetCodeDaily/LeetCode/FindPivotIndex724_Easy.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode724;//solved
+
+public class Solution
+{
+    public int PivotIndex(int[] nums)
+    {
+        int left = 0;
+        int right = nums.Sum();
+ 
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(i > 0)
+                left += nums[i-1];
+            right -= nums[i];
+
+            if (right == left)
+                return i;
+        }
+        return -1;
+    }
+}
