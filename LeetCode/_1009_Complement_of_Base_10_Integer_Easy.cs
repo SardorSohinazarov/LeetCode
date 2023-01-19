4dc678187903f3305697fed604af44008bfaf3a6
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-01-20 04:05:04 +0500

    solved

---
+++ LeetCode/_1009_Complement_of_Base_10_Integer_Easy.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode1009//solved
+{
+    public class Solution
+    {
+        public int BitwiseComplement(int n)
+        {
+            return n ^ ((1 << Convert.ToString(n, 2).Length) - 1);
+        }
+    }
+}
