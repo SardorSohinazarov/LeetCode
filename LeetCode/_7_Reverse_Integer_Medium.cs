396c6cda3aa2801c99388b7cbbf62ce19aaf776e
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-01-23 13:21:15 +0500

    7-ni yechdim

---
+++ LeetCode/_7_Reverse_Integer_Medium.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode7;//solved
+
+public class Solution
+{
+    public int Reverse(int x)
+    {
+        long result = 0;
+
+        while (x != 0)
+        {
+            result = result * 10 + x % 10;
+            x /= 10;
+        }
+
+        if (result > int.MaxValue || result < int.MinValue)
+            return 0;
+
+        return (int)result;
+    }
+}
