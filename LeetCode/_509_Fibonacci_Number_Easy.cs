3f8e02cf18a13dd3d75eddfe54544b8da6a53d3e
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-01-23 13:52:33 +0500

    509 yechdim

---
+++ LeetCode/_509_Fibonacci_Number_Easy.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode509//solved
+{
+    public class Solution
+    {
+        public int Fib(int n)
+        {
+            if (n > 1)
+                return Fib(n - 1) + Fib(n - 2);
+            return n;
+        }
+    }
+}
