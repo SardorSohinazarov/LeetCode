2711aab9cb203203a1133348e324bc46e6559da2
Sardor Sohinazarov <sardorstudent0618@gmail.com>
2023-02-01 02:38:00 +0500

    367 ketdi

---
+++ LeetCode/_367_Valid_Perfect_Square_Easy.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode367
+{
+    public class Solution
+    {
+        public bool IsPerfectSquare(int num)
+        {
+            long x = num;
+
+            while(x*x > num)
+            {
+                x = (x + num / x) / 2;
+            }
+
+            return x * x == num;
+        }
+    }
+
+    /* public class Solution
+     {
+         public bool IsPerfectSquare(int num)
+         {
+             int i = 1;
+             while(num > 0)
+             {
+                 num -= i;
+                 i += 2;
+             }
+
+             return num == 0;
+         }
+     }*/
+}
